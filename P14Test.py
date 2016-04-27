4f9baed0608c5c2340765d6991d434fa16ada3ce
karmaTsering <gurungkarm@mnstate.edu>
2016-04-27 16:04:31 -0500

    Update P14Test.py

--- P14Test.py
+++ P14Test.py
@@ -2,6 +2,19 @@
 Description:Test for modCustomSet.py
 """
 from modCustomSet import*
-Set=[1,2,3,4,5,5,5,5,1]
+print("Tester for Set")
+Set1=CustomSet(1,2,3,4,5,5,5,5,1)
+Set2=CustomSet()
+print(Set1)
+print()
+print(Set2)
+print("-"*80)
+print("Tester for Unio Set")
+print("It should print: [1,2,3,4,5]")
+set3=CustomSet([1,3,5,1])
+set4=CustomSet([1,2,3,4])
+set5=set3+set4
+print(set5)
+
 
 
